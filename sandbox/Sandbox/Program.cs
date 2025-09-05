b8fda8858b6aa737eebdd9fc4a39833b46623a0d
samuelmaciel015 <samuel.amorimmaciel015@gmail.com>
2025-09-05 08:37:55 -0300

    Exercise 1 completed

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!!!");
+        Console.WriteLine("This is in C#.");
+
+        Console.Write("Whats your favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your color is {color}");
+
     }
 }
