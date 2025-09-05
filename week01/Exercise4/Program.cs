b1808bcb327128e042da2041392dd894576a8449
samuelmaciel015 <samuel.amorimmaciel015@gmail.com>
2025-09-05 20:36:25 -0300

    Exercise 4 completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            string enterNumber = Console.ReadLine();
+            number = int.Parse(enterNumber);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+
+        int sum = 0;
+        int average = 0;
+        int largest = 0;
+
+        foreach (int i in numbers)
+        {
+            sum += i;
+            average = sum / numbers.Count;
+
+            if (largest < i)
+            {
+                largest = i;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
