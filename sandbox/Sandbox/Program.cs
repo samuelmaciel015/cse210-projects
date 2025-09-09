5951952ad1cdee2f8b1da9c6618429ad845e333d
samuelmaciel015 <samuel.amorimmaciel015@gmail.com>
2025-09-08 21:26:02 -0300

    Resume completed

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,24 +1,10 @@
 using System;
-
+/**/
 class Program
 {
     static void Main(string[] args)
     {
-        int x = 5;
-        int y = 2;
 
-        if (x > y)
-        {
-            Console.WriteLine("Greater");
-        }
-        else if (x < y)
-        {
-            Console.WriteLine("Less");
-        }
-        else
-        {
-            Console.WriteLine("Equal");
-        }
 
     }
 }
