fc579380d93565990e72f78f940b06d4e7e2a69d
samuelmaciel015 <samuel.amorimmaciel015@gmail.com>
2026-01-07 20:02:34 -0300

    Exercise 2 completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string strPercentage = Console.ReadLine();
+
+        int percentage = int.Parse(strPercentage);
+        string letter;
+        string sign = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (percentage % 10 >= 7 && (letter != "A" && letter != "F"))
+        {
+            sign = "+";
+        }
+        else if (percentage % 10 < 3 && (letter != "F"))
+        {
+            sign = "-";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{sign}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You pass!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you didnt pass, lucky next time!");
+        }
     }
 }
