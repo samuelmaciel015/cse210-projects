2db57078fa5da4da0c216a1386657384c8130538
samuelmaciel015 <samuel.amorimmaciel015@gmail.com>
2026-01-08 18:33:19 -0300

    Exercise 5 completed

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string strNumber = Console.ReadLine();
+            int number = int.Parse(strNumber);
+            return number;
+        }
+
+        int SquareNumber (int number)
+        {
+            int squared = number * number;
+            return squared;
+        }
+
+        void DisplayResult (string name, int squared)
+        {
+            Console.WriteLine($"{name}, the square of your number is {squared}");
+        }
+
+        DisplayWelcome();
+        DisplayResult(PromptUserName(), SquareNumber(PromptUserNumber()));
     }
 }
